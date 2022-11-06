84293b79172224410adc1ae46721b3013e826de8
ruviter <turtlesim999@gmail.com>
2022-11-07 08:37:26 +0900

    list, item 수정

--- src/components/Detail/Route/Route.jsx
+++ src/components/Detail/Route/Route.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./Route.module.css";
-import route from '../../../img/route.png'
+import route from "../../../img/route.png";
 
 function Route(props) {
   return (
     <div>
       <div className={styles.title}>가는 경로</div>
-      <img
-        className={styles.img}
-        src={route}      />
+      <img className={styles.img} src={route} />
     </div>
   );
 }
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -19,19 +19,17 @@ function List({ onDetail, onCompare }) {
 function Search({ onCompare }) {
   return (
     <>
-      <form style={{ display: "flex", margin: "10px 10px" }} action="submit">
-        <input style={{ flex: "1" }} type="text" />
+      <form className={styles.form} action="submit">
+        <input type="text" />
         <button>
           <IoIosSearch />
         </button>
       </form>
-      <div style={{ color: "black", fontSize: "0.8rem", marginLeft: "10px" }}>
-        <strong style={{ color: "var(--color-accent)" }}>낮은 가격순</strong> /
-        높은 가격순 / 가까운 거리순 / 별점 높은순
+      <div className={styles.classify}>
+        <strong>낮은 가격순</strong> / 높은 가격순 / 가까운 거리순 / 별점 높은순
       </div>
-      <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+      <div className={styles.compare}>
         <button
-          className={styles.detail}
           onClick={() => {
             onCompare(true);
           }}
@@ -46,28 +44,28 @@ function Search({ onCompare }) {
 const inf = [
   {
     a: "A",
-    title: "서울 자동차운전면허학원",
+    title: "서울 자동차운전전문학원",
     star: "★★★☆☆",
     price: "79",
     distance: "1.2",
   },
   {
     a: "B",
-    title: "명학 자동차운전면허학원",
+    title: "명학 자동차운전전문학원",
     star: "★★★★☆",
     price: "80",
     distance: "2",
   },
   {
     a: "C",
-    title: "신현대 자동차운전면허학원",
+    title: "신현대 자동차운전전문학원",
     star: "★★★☆☆",
     price: "81.2",
     distance: "1.5",
   },
   {
     a: "D",
-    title: "단원 자동차운전면허학원",
+    title: "단원 자동차운전전문학원",
     star: "★★★☆☆",
     price: "82.2",
     distance: "1.3",
