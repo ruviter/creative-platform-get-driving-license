f0e910730cf9399bf5e7e1e4b88b165a007b1ab1
ruviter <turtlesim999@gmail.com>
2022-11-07 09:00:44 +0900

    detail 수정2

--- src/components/Detail/People/People.jsx
+++ src/components/Detail/People/People.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import styles from './People.module.css'
-import Person from './Person/Person';
+import React from "react";
+import styles from "./People.module.css";
+import Person from "./Person/Person";
 
 function People(props) {
-    return (
-        <>
-            <div className={styles.title}>강사정보</div>
-        <div className={styles.people}>
-            <Person/>
-            <Person/>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.title}>강사정보</div>
+      <div className={styles.people}>
+        {inf.map((p) => (
+          <Person inf={p} />
+        ))}
+      </div>
+    </>
+  );
 }
+const inf = [
+  {
+    img: "https://mblogthumb-phinf.pstatic.net/20150122_225/zikil337_1421903667630tY9OM_JPEG/1406712927189_1406709118496-1.jpg?type=w2",
+    name: "우민혁",
+    star: "★ ★ ★ ★ ★",
+  },
+  {
+    img: "https://mblogthumb-phinf.pstatic.net/20150122_225/zikil337_1421903667630tY9OM_JPEG/1406712927189_1406709118496-1.jpg?type=w2",
+    name: "심준석",
+    star: "★ ★ ★ ★ ★",
+  },
+  {
+    img: "https://mblogthumb-phinf.pstatic.net/20150122_225/zikil337_1421903667630tY9OM_JPEG/1406712927189_1406709118496-1.jpg?type=w2",
+    name: "김선혁",
+    star: "★ ★ ★ ★ ★",
+  },
+  {
+    img: "https://mblogthumb-phinf.pstatic.net/20150122_225/zikil337_1421903667630tY9OM_JPEG/1406712927189_1406709118496-1.jpg?type=w2",
+    name: "김한식",
+    star: "★ ★ ★ ★ ★",
+  },
+];
 
 export default People;
--- src/components/Detail/People/Person/Person.jsx
+++ src/components/Detail/People/Person/Person.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Person.module.css'
 
-function Person(props) {
+function Person({inf}) {
     const imgSrc = 'https://file.mk.co.kr/meet/neds/2021/07/image_readmed_2021_659579_16257086594710103.jpg'
-
+    const {img,name,star} = inf
     return (
         <div className={styles.person}>
-            <img className={styles.img} src={imgSrc} alt="profile" />
-            <div>김지수 강사님</div>
-            <div className={styles.star}>★ ★ ★ ★ ★</div>
+            <img className={styles.img} src={img} alt="profile" />
+            <div>{name} 강사</div>
+            <div className={styles.star}>{star}</div>
             <button >상세리뷰</button>
         </div>
     );
