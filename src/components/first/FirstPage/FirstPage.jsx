cbb194ccb0ca918dc4936077e6f2058de10e206b
ruviter <turtlesim999@gmail.com>
2022-11-08 20:46:12 +0900

    firstpage

--- src/components/first/FirstPage/FirstPage.jsx
+++ src/components/first/FirstPage/FirstPage.jsx
@@ -2,51 +2,37 @@ import React from "react";
 import License from "../License/License";
 import Location from "../Location/Location";
 import styles from "./FirstPage.module.css";
-import map from '../../../img/map.png'
+import map from "../../../img/map.png";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import Info from "../Info/Info";
 
 function FirstPage({ onNextPage }) {
- 
   return (
     <div className={styles.container}>
+      <div className={styles.containerin}>
       <section className={styles.sectionIntro}>
         <Intro />
       </section>
-      <section className={styles.info}>
-        <div className={styles.loc}>
-          <Location />
-        </div>
-        <div className={styles.license}>
-          <License />
-        </div>
+      <section className={styles.sectionInfo}>
+        <Info/>
       </section>
-      <button
-        className={styles.move}
-        onClick={() => {
-          onNextPage(2);
-        }}
-      >
-        <BsFillArrowRightCircleFill />
-      </button>
+      </div>
     </div>
   );
 }
 
 function Intro() {
-  const imgSrc =
-  "https://velog.velcdn.com/images%2Fminn602%2Fpost%2Fb49932ff-9d9c-4711-a4df-73f2e6111fc4%2Fimage.png";
-    return (
+  return (
     <>
       <div className={styles.intro}>
-        <p style={{ marginBottom: "7px", fontSize: "1.3rem" }}>운전면허딸땐,</p>
+        <p className={styles.subtitle}>운전면허딸땐,</p>
         <div className={styles.main}> 면허따자</div>
-        <p style={{ fontSize: "1.2rem" }}>
-          가격비교 + 위치비교 + 신뢰도 있는 리뷰 ={" "}
+        <p style={{ fontSize: "1.2rem", lineHeight:'0.7rem' }}>
+          가격비교 + 위치비교 + 신뢰도 있는 후기 ={" "}
           <span style={{ fontWeight: "900" }}>면허따자</span>
-          <p>어떤 학원을 갈지 천천히 골라봐~</p>
+          <p>어떤 학원을 갈지 골라봐~</p>
         </p>
       </div>
-      <img className={styles.img} src={map} />
     </>
   );
 }
---
+++ src/components/first/Info/Info.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from './Info.module.css'
+function Info(props) {
+  return (
+    <>
+      <form action="submit" className={styles.form}>
+        <input type="text" placeholder="사는곳을 입력하면 근처 학원을 알아볼게요"/>
+        <div>현재위치로 하고 싶다면 위치정보서비스에 동의해주세요</div>
+      </form>
+      <div className={styles.license}>어떤 운전면허를 따시겠어요?</div>
+      <div className={styles.buttons}>
+        <button className={styles.j1}>1종 운전면허</button>
+        <button className={styles.j2}>2종 운전면허</button>
+      </div>
+      <button className={styles.next}>운전면허학원 알아보기</button>
+    </>
+  );
+}
+
+export default Info;
