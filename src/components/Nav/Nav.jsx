92bd69d9090c0a50c6fbff0b3272097323f75824
ruviter <turtlesim999@gmail.com>
2022-11-14 22:33:49 +0900

    add review window

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -56,7 +56,7 @@ function Login({ onClose, setAuth }) {
       <form action="submit">
         <input type="text" placeholder="아이디를 입력하세요" />
         <input type="password" placeholder="비밀번호를 입력하세요" />
-        <button></button>
+        <button>로그인</button>
       </form>
     </div>
   );
--- src/components/second/Detail/People/Person/Person.jsx
+++ src/components/second/Detail/People/Person/Person.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import styles from './Person.module.css'
+import React from "react";
+import { useState } from "react";
+import styles from "./Person.module.css";
 
-function Person({inf}) {
-    const imgSrc = 'https://file.mk.co.kr/meet/neds/2021/07/image_readmed_2021_659579_16257086594710103.jpg'
-    const {img,name,star} = inf
-    return (
-        <div className={styles.person}>
-            <img className={styles.img} src={img} alt="profile" />
-            <div>{name} 강사</div>
-            <div className={styles.star}>{star}</div>
-            <button >상세리뷰</button>
-        </div>
-    );
+function Person({ inf }) {
+  const imgSrc =
+    "https://file.mk.co.kr/meet/neds/2021/07/image_readmed_2021_659579_16257086594710103.jpg";
+  const { img, name, star } = inf;
+  const [review, setReview] = useState(false);
+  return (
+    <div className={styles.person}>
+      <img className={styles.img} src={img} alt="profile" />
+      <div>{name} 강사</div>
+      <div className={styles.star}>{star}</div>
+      <button
+        onClick={() => {
+          setReview(true);
+        }}
+      >
+        상세리뷰
+      </button>
+      {review && <Review onClose={setReview} />}
+    </div>
+  );
+}
+
+function Review({ onClose }) {
+  return (
+    <div className={styles.container}>
+      <button className={styles.close} onClick={() => onClose(false)}>
+        X
+      </button>
+      <ul>
+        <li>
+            <div>아이디 : 수다쟁이</div>
+            <div>
+            <span>별점: </span> <span>영수증 인증한 리뷰</span>
+            </div>
+            <div>너무 친절하게 잘 대해주셔서 한번에 합격했어요..!! 진짜 강추!!</div>
+
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default Person;
