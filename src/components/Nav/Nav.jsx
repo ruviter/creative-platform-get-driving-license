f3ed66f459434ad99309d567aba3a38250ea25c3
ruviter <turtlesim999@gmail.com>
2022-11-15 09:37:52 +0900

    login window design

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -47,15 +47,35 @@ function Nav({ setPage }) {
   );
 }
 
-function Login({ onClose, setAuth }) {
+function Login({ onClose, onAuth }) {
+  const [warn, setWarn] = useState(null);
+  const onLogin = (e) => {
+    e.preventDefault();
+    const id = e.target[0].value;
+    const pw = e.target[1].value;
+    if (pw === "") {
+      setWarn("패스워드를 입력하세요");
+    }
+    if (id === "") {
+      setWarn("아이디를 입력하세요");
+    }
+
+    if (id === "asdf" && pw === "1234") {
+      console.log("login!");
+      onAuth(true);
+      onClose(false);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <button className={styles.close} onClick={() => onClose(false)}>
         X
       </button>
-      <form action="submit">
+      <form action="submit" className={styles.form} onSubmit={onLogin}>
         <input type="text" placeholder="아이디를 입력하세요" />
         <input type="password" placeholder="비밀번호를 입력하세요" />
+        <div>{warn}</div>
         <button>로그인</button>
       </form>
     </div>
