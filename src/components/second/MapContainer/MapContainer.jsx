5d724a55ef431d03b56caa7a684d9eb019a62eed
ruviter <turtlesim999@gmail.com>
2022-11-16 11:09:14 +0900

    다시시작 map 현재 위치 초기 설정, map 생성

--- src/components/second/MapContainer/MapContainer.jsx
+++ src/components/second/MapContainer/MapContainer.jsx
@@ -6,17 +6,17 @@ import { list } from "../incheonSchoolList/list";
 const { kakao } = window;
 
 function MapContainer({ center }) {
-  const [currentL, setCurrentL] = useState({});
+  const [currentL, setCurrentL] = useState(defaultLocation);
   const [currentMarker, setCurrentMarker] = useState({})
   const [map, setMap] = useState({});
   useEffect(() => {
-    createMap(setMap);
+    createMap(setMap, currentL);
   }, []);
   useEffect(() => {
-    getCurrentLocation(map, setCurrentL);
-    createCurrentMarker(map, currentL, setCurrentMarker);
-    schoolMarkers(map);
-    searchMap("내손로 14", map);
+    // getCurrentLocation(map, setCurrentL);
+    // createCurrentMarker(map, currentL, setCurrentMarker);
+    // schoolMarkers(map);
+    // searchMap("내손로 14", map);
   }, [map]);
   return (
     <>
@@ -29,36 +29,26 @@ function MapContainer({ center }) {
   );
 }
 
-const imageSrc =
-  "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-const imageSize = new kakao.maps.Size(24, 35);
-const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-const createMap = (setMap) => {
+const createMap = (setMap, currentL) => {
   const container = document.getElementById("myMap");
   const options = {
-    center: new kakao.maps.LatLng(33.45, 126.57),
+    center: currentL.kakaoLatLng,
     level: 6,
   };
   const map = new kakao.maps.Map(container, options);
   setMap(map);
 };
 
-const getCurrentLocation = (map, setCurrentL) => {
+const getCurrentLocation = (setCurrentL) => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
-      setCurrentL({ lat, lon });
-      const locPosition = new kakao.maps.LatLng(lat, lon);
-
-      map.setCenter(locPosition);
+      const kakaoLatLng = new kakao.maps.LatLng(lat, lon);
+      
+      setCurrentL({ lat, lon, kakaoLatLng });
     });
-  } else {
-    const lat = 37.3817847;
-    const lon = 126.6677834;
-    const locPosition = new kakao.maps.LatLng(lat, lon);
-    setCurrentL({ lat, lon });
   }
 };
 
@@ -77,11 +67,11 @@ const schoolMarkers = (map) => {
   Object.keys(list).map((name) => {
     const ps = new kakao.maps.services.Places();
     ps.keywordSearch(name, placesSearchCB);
-
+    
     function placesSearchCB(data, status, pagination) {
       const iwContent = `<div>${name}</div>`;
       const infowindow = new kakao.maps.InfoWindow({ content: iwContent });
-
+      
       if (status === kakao.maps.services.Status.OK) {
         const markerPosition = new kakao.maps.LatLng(data[0].y, data[0].x);
         const marker = new kakao.maps.Marker({
@@ -98,22 +88,28 @@ const schoolMarkers = (map) => {
 
 const searchMap = (keyword, map) => {
   const geocoder = new kakao.maps.services.Geocoder();
-
+  
   geocoder.addressSearch(keyword, (result, status) => {
     // 검색결과 마크 띄우기
     if (status === kakao.maps.services.Status.OK) {
       const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
+      
       const marker = new kakao.maps.Marker({ map: map, position: coords });
-
+      
       const infowindow = new kakao.maps.InfoWindow({
         content:
-          '<div style="width:150px;text-align:center;padding:6px 0;">우리 집</div>',
+        '<div style="width:150px;text-align:center;padding:6px 0;">우리 집</div>',
       });
-
       infowindow.open(map, marker);
     }
   });
 };
+
+const imageSrc =
+"https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+const imageSize = new kakao.maps.Size(24, 35);
+const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+const defaultLocation = { lat:37.3817847, lon:126.6677834, kakaoLatLng : new kakao.maps.LatLng(37.3817847, 126.6677834)};
 
 export default MapContainer;
