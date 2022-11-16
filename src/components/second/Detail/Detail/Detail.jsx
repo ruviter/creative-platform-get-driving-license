60ed69412c29b73524469d7f6dfc4a761cf39524
ruviter <turtlesim999@gmail.com>
2022-11-16 22:59:39 +0900

    아이콘 세부정보 다듬기

--- src/components/second/Detail/Detail/Detail.jsx
+++ src/components/second/Detail/Detail/Detail.jsx
@@ -32,22 +32,36 @@ function Detail({ onClose }) {
 }
 function Details() {
   return (
-    <div style={{ flex: "1" }}>
+    <div className={styles.detailsContainer} style={{ flex: "1" }}>
       <div className={styles.title}>세부정보</div>
       <p>상세주소 : 경기 의왕시 경수대로 321</p>
       <p>영업시간 : 오전 9 : 00 ~ 오후 20 : 00</p>
       <p>전화번호 : 031-325-3405</p>
       <p>홈페이지 : https://encycolorpedia.kr/808080</p>
-      <p>
-        <>
+      <p className={styles.icons}>
+        <span>
           <FaBus />
-        </>
-        <>
+           
+        </span>
+        <span>
           <SlUserFemale />
-        </>
-        <>
+           
+        </span>
+        <span>
           <MdOutlineNightsStay />
-        </>
+          
+        </span>
+      </p>
+      <p className={styles.iconsDesc}>
+        <span>
+          셔틀버스 보유
+        </span>
+        <span>
+          여성강사 보유
+        </span>
+        <span>
+          야간수업 존재
+        </span>
       </p>
     </div>
   );
